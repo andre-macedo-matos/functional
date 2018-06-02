75f60eff0f6bab9e9f751dcecfe5b2eaee7882be
andre macedo <andre@macedo>
2018-06-02 19:47:38 -0300

    [inspecao] criado classe de teste

--- functional/src/test/java/br/com/atf/functional/model/inspect/test/InspectPage.java
+++ functional/src/test/java/br/com/atf/functional/model/inspect/test/InspectPage.java
@@ -1,12 +1,25 @@
 package br.com.atf.functional.model.inspect.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
+
+import br.com.atf.functional.model.Portal;
 
 public class InspectPage {
 	
 	@Test
-	public void shouldRedirectToLoginPage() {
+	public void shouldGetLoginForm() {		
+		Portal portal = new Portal("http://localhost:8080/livraria");
+		Map<String, List<String>> components = portal.getNavigationElements();
 		
+		int actualComponentsQuantity = components.size();
+		assertEquals(2, actualComponentsQuantity);
 	}
+
+	
 
 }
