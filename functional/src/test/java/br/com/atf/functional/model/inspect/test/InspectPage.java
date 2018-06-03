3a95425838957f6e34c11ed915ba485da54c640f
andre macedo <andre@macedo>
2018-06-02 22:01:36 -0300

    [setup][inspecao] incluido pastas de drives, teste do BDD IT2

--- functional/src/test/java/br/com/atf/functional/model/inspect/test/InspectPage.java
+++ functional/src/test/java/br/com/atf/functional/model/inspect/test/InspectPage.java
@@ -2,22 +2,22 @@ package br.com.atf.functional.model.inspect.test;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.List;
 import java.util.Map;
 
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 
 import br.com.atf.functional.model.Portal;
 
 public class InspectPage {
 	
 	@Test
-	public void shouldGetLoginForm() {		
+	public void whenGivenAValidUrlShouldReturnLoginForm() {		
 		Portal portal = new Portal("http://localhost:8080/livraria");
-		Map<String, List<String>> components = portal.getNavigationElements();
+		Map<WebElement, Object> components = portal.getNavigationElements();
 		
-		int actualComponentsQuantity = components.size();
-		assertEquals(2, actualComponentsQuantity);
+		int actualAttributesQuantity = components.size();
+		assertEquals(2, actualAttributesQuantity);
 	}
 
 	
