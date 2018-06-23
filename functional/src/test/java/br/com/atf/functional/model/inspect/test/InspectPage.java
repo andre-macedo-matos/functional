cbc0e2732c056e974ba49abdb1f2569fa7baa66c
andre macedo <andre@macedo>
2018-06-23 17:38:46 -0300

    [inspecao] implementado testes do hibernate validator

--- functional/src/test/java/br/com/atf/functional/model/inspect/test/InspectPage.java
+++ functional/src/test/java/br/com/atf/functional/model/inspect/test/InspectPage.java
@@ -5,14 +5,26 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+
+import org.junit.Before;
 import org.junit.Test;
-'
-import br.com.atf.functional.exception.UnreachablePageException;
+
 import br.com.atf.functional.model.NavigationElement;
 import br.com.atf.functional.model.Portal;
 
 public class InspectPage {
+
+	private Validator validator;
+	
+	@Before
+	public void setup() {
+		this.validator = Validation.buildDefaultValidatorFactory().getValidator();
+	}
 
 	@Test
 	public void whenGivenAValidUrlShouldReturnLoginForm() {
@@ -35,25 +47,32 @@ public class InspectPage {
 			assertTrue(doesContainsValues);
 		}
 	}
-
-	@Test(expected=UnreachablePageException.class)
-	public void whenGivenAnInvalidUrlReturnException() {
-		Portal portal = new Portal("http://localhost:999/livra");
-		portal.inspectElements();
-	}
 	
 	@Test
-	public void whenGivenAnEmptydUrlReturnException() {
-		Portal portal = new Portal("");
-		portal.inspectElements();
+	public void whenNotGivenUrlReturnErrorMessage() {
+		Portal portal = new Portal(null);
+		Set<ConstraintViolation<Portal>> constraintViolations = validator.validate(portal);
+		
+		int quantityOfErrors = constraintViolations.size();
+		String actualMessage = constraintViolations.iterator().next().getMessage();
+		String expectedMessage = "Por favor, informe um endereço para realizar inspeção.";
+		
+		assertEquals(1, quantityOfErrors);
+		assertEquals(expectedMessage, actualMessage);
 	}
-	
-	@Test//(expected=RedirectPageException.class)
-	public void whenGivenAValidUrlWichIsRedirectReturnException() {
 
-		Portal portal = new Portal("http://localhost:999/livraria");
-		portal.inspectElements();
-
+	@Test
+	public void whenGivenAValidUrlWhichIsRedirectReturnErrorMessage() {
+		String url = "http://localhost:999/livraria";
+		Portal portal = new Portal(url);
+		Set<ConstraintViolation<Portal>> constraintViolations = validator.validate(portal);
+		
+		int quantityOfErrors = constraintViolations.size();
+		String expected = "O endereço " + url + " redirecionou para " + url + "/login?1";
+		String validatorMessage = constraintViolations.iterator().next().getMessage();
+		
+		assertEquals(1, quantityOfErrors);
+		assertEquals(expected, validatorMessage);
 	}
 
 }
